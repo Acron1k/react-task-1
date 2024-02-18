4a578f9b065a04ee2de502be4637e273ea8166f9
Acron1k <danilm.5@yandex.ru>
2024-02-18 21:17:43 +0500

    Без использования JSX

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,36 @@
 import logo from './logo.svg'; //императивный стиль
 import './App.css'; //императивный стиль
+import { createElement } from 'react';
 
 export const App = () => {
 	const currentYear = new Date().getFullYear();
-	return (
-		<div className="App">
-			{/*декларативный стиль*/}
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" /> {/*декларативный стиль*/}
-				<p>Hello World!</p>
-				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-					Learn React
-				</a>
-				<p>{currentYear}</p>
-			</header>
-		</div>
+	return createElement(
+		'div',
+		{
+			className: 'App',
+		},
+		createElement(
+			'header',
+			{
+				className: 'App-header',
+			},
+			createElement('img', {
+				src: logo,
+				className: 'App-logo',
+				alt: 'logo',
+			}),
+			createElement('p', null, 'Hello World!'),
+			createElement(
+				'a',
+				{
+					className: 'App-link',
+					href: 'https://reactjs.org',
+					target: '_blank',
+					rel: 'noopener noreferrer',
+				},
+				'Learn React',
+			),
+			createElement('p', null, currentYear),
+		),
 	);
 };
