22093871a57761eff9899896885f5f89696685fb
Acron1k <danilm.5@yandex.ru>
2024-02-18 21:01:07 +0500

    Использование JSX + комменты

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'; //императивный стиль
+import './App.css'; //императивный стиль
 
 export const App = () => {
 	const currentYear = new Date().getFullYear();
 	return (
 		<div className="App">
+			{/*декларативный стиль*/}
 			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
+				<img src={logo} className="App-logo" alt="logo" /> {/*декларативный стиль*/}
 				<p>Hello World!</p>
 				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
 					Learn React
